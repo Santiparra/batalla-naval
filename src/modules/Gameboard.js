015796fe0dd0c8e0ce5ddb492a1473e2ca75e030
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-02-10 14:13:13 -0300

    Modulo gameboard completo con sus tests correspondientes

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -3,6 +3,11 @@ import { Ship } from "./Ships"
 class GameBoard {
     constructor (squares) {
         this.squares = []
+        this.Carrier = new Ship("Carrier",5,0,false);
+        this.Battleship = new Ship("Battleship",4,0,false);
+        this.Cruiser = new Ship("Cruiser",3,0,false);
+        this.Submarine = new Ship("Submarine",3,0,false);
+        this.Destroyer = new Ship("Destroyer",2,0,false);
     }
     createBoard () {
         for (let x = 0; x < 10; x++) {
@@ -12,29 +17,21 @@ class GameBoard {
         }return this.squares
     }
 
-    /* buildIndex (arrayCCs) {
-        return Number (arrayCCs.join(''))
-    } */
-    createShips () {
-        const Carrier = new Ship("Carrier",5,0,false);
-        const Battleship = new Ship("Battleship",4,0,false);
-        const Cruiser = new Ship("Cruiser",3,0,false);
-        const Submarine = new Ship("Submarine",3,0,false);
-        const Destroyer = new Ship("Destroyer",2,0,false);
-    }
-
     placeShip (shipName, shipSize, arrayCCs, direction) {
         const index = Number (arrayCCs.join(''))
-        for (let i = 0; i < shipSize; i++) {
+        let vIndex = index
             if (direction === "h") {
-                this.squares.splice(index+i, shipSize, shipName)
+                for (let i = 0; i < shipSize; i++) {
+                    this.squares.splice(index+i, 1, shipName)                    
+                }
             } else if (direction === "v") {
-                this.squares.splice((index+i)+10, shipSize, shipName)
-            }
-        }
+                for (let i = 0; i < shipSize; i++) {
+                    this.squares.splice(vIndex, 1, shipName)
+                    vIndex+=10                                   
+                }                 
+            }        
         return this.squares
     } 
-
 
     //takes a pair of coordinates, determines whether or not 
     //the attack hit a ship and then sends the ‘hit’ function to 
@@ -44,20 +41,40 @@ class GameBoard {
         if (this.squares[location].length == 2) {
             this.squares.splice(location, 1, "water")
         } else if (this.squares[location] === "Carrier") {
-            this.squares.splice(location, 1, "hit")
-        } else if (this.squares[location] === "Battleship") {
-            this.squares.splice(location, 1, "hit")
-        } else if (this.squares[location] === "Cruiser") {
-            this.squares.splice(location, 1, "hit")
-        } else if (this.squares[location] === "Submarine") {
-            this.squares.splice(location, 1, "hit")
-        } else if (this.squares[location] === "Destroyer") {
-            this.squares.splice(location, 1, "hit")
-        }
-        
-        
-    }
+            this.Carrier.hit(1);
+            if (this.Carrier.isSunk(this.Carrier.length, this.Carrier.hits.length) == false) {
+                this.squares.splice(location, 1, "hit")
+            }  else {this.squares.splice(location, 1, "sunk")}
 
+        } else if (this.squares[location] === "Battleship") {
+            this.Battleship.hit(1);
+            if (this.Battleship.isSunk(this.Battleship.length, this.Battleship.hits.length) == false) {
+                this.squares.splice(location, 1, "hit")
+            }  else {this.squares.splice(location, 1, "sunk")}
+        } else if (this.squares[location] === "Cruiser") {
+            this.Cruiser.hit(1);
+            if (this.Cruiser.isSunk(this.Cruiser.length, this.Cruiser.hits.length) == false) {
+                this.squares.splice(location, 1, "hit")
+            }  else {this.squares.splice(location, 1, "sunk")}            
+        } else if (this.squares[location] === "Submarine") {
+            this.Submarine.hit(1);
+            if (this.Submarine.isSunk(this.Submarine.length, this.Submarine.hits.length) == false) {
+                this.squares.splice(location, 1, "hit")
+            }  else {this.squares.splice(location, 1, "sunk")}            
+        } else if (this.squares[location] === "Destroyer") {
+            this.Destroyer.hit(1);
+            if (this.Destroyer.isSunk(this.Destroyer.length, this.Destroyer.hits.length) == false) {
+                this.squares.splice(location, 1, "hit")
+            }  else {this.squares.splice(location, 1, "sunk")}            
+        }            
+    }  
+}
+
+/* buildIndex (arrayCCs) {
+        return Number (arrayCCs.join(''))
+    } 
+     
+    
     itinerateArr () {
         for (let i = 0; i < this.squares.length; i++) {
             if (this.squares[i] ) {
@@ -66,5 +83,8 @@ class GameBoard {
             
         }
     }
-}
+        
+    
+    */
+
 export default GameBoard
--- src/modules/Gameboard.test.js
+++ src/modules/Gameboard.test.js
@@ -9,12 +9,12 @@
     test('are boards squares in right order', () => {
         expect(newBoard.squares[67]).toStrictEqual([6, 7])
     })
-     /* test("are ships being added at right index", () => {
-        expect(newBoard.buildIndex([6,9])).toBe(69)
-    })  */
     test("are ships being added at right horizontal index", () => {
         newBoard.placeShip ("Submarine",3, [2,3], "h")
         expect(newBoard.squares[25]).toEqual("Submarine")
+    })
+    test('are boards being handled well', () => {
+        expect(newBoard.squares.length).toBe(100)
     })
     test("are ships being added at right vertical index", () => {
         newBoard.placeShip ("Destroyer",2, [3,4], "v")
@@ -25,8 +25,12 @@
         expect(newBoard.squares[91]).toStrictEqual("water")
     })
       test("are ships being hitted", () => {
-        newBoard.receiveAttack ([2,3])
-        expect(newBoard.squares[23]).toStrictEqual("hit")
+        newBoard.receiveAttack ([3,4])
+        expect(newBoard.squares[34]).toStrictEqual("hit")
+    })  
+     test("are ships being sunked", () => {
+        newBoard.receiveAttack ([4,4])
+        expect(newBoard.squares[44]).toStrictEqual("sunk")
     })  
 
 
